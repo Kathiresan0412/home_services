b90a0e17c5ba6884e8cc9000018b6c591e3affcd
sangeethasanthiralingam <sangeethasanthiralingam@gmail.com>
2024-01-26 14:32:06 +0530

    commit

--- lib/onboardings/one.dart
+++ lib/onboardings/one.dart
@@ -12,15 +12,17 @@ class _OnboardingOneState extends State<OnboardingOne> {
   Widget build(BuildContext context) {
     double minSide = MediaQuery.of(context).size.shortestSide * 0.1;
 
-    return SafeArea(
-      child: Stack(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Positioned(
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Stack(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Positioned(
                       top: 0,
                       left: 0,
                       child: SizedBox(
@@ -33,23 +35,72 @@ class _OnboardingOneState extends State<OnboardingOne> {
                             fit: BoxFit.cover,
                           ),
                         ),
-                      )),
-                ],
-              ),
-              const Column(
-                children: [
-                  SizedBox(
-                      child: Text('skip',
-                          style: TextStyle(
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                          ))),
-                ],
-              )
-            ],
-          ),
-          // Add more widgets to the Stack if needed
-        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: minSide * 0.35),
+                      child: SizedBox(
+                        width: minSide * 1.75,
+                        height: minSide * 0.75,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.teal[200],
+                            borderRadius: BorderRadius.circular(minSide * 0.5),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'skip',
+                              style: TextStyle(
+                                color: Colors.black,
+                                decoration: TextDecoration.none,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        color: Colors.yellow,
+                        width: minSide * 6,
+                        height: minSide * 9,
+                        child: Center(
+                          child: Container(
+                            color: Colors.red[700],
+                            width: minSide * 4,
+                            height: minSide * 6,
+                            child: Center(
+                              child: SizedBox(
+                                child: Image.asset(
+                                  'images/image_girl.jpg',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            // Add more widgets to the Stack if needed
+          ],
+        ),
       ),
     );
   }
